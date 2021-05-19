65aa0950aed45e5d22a2989aa5b90c2bff90ed60
takuya20201003 <wsxedc789456@gmail.com>
2021-05-19 22:24:00 +0900

    ユーザー登録画面のHTMLの作成

---
+++ app/models/holiday.rb
@@ -0,0 +1,7 @@
+class Holiday < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '土日祝' },
+    { id: 3, name: '不定休' }
+  ]
+  end
---
+++ app/models/level.rb
@@ -0,0 +1,10 @@
+class Level < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '上級' },
+    { id: 3, name: '中上級' },
+    { id: 4, name: '中級' },
+    { id: 5, name: '初中級' },
+    { id: 6, name: '初級' }
+  ]
+end
