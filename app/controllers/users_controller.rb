d1fe3756fe69cf3c06ac159223a0bf602605b44f
takuya20201003 <wsxedc789456@gmail.com>
2021-05-26 23:17:51 +0900

    usersコントローラーへの記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
+
+  before_action :search_user, only: [:index,:search]
+  
   def index
+    @users = Users.all
   end
 
   def search
+    @result = @u.includes(:category)
   end
+
+  private
+
+  def search_product
+    @u = User.ransack(params[:u])
+  end
+  
 end
